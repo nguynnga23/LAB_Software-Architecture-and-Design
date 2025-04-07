2e614d9f78adddfc99e40f49d48558807dfcdf14
Nguyen Nga <nguyennga200x@gmail.com>
2025-04-07 19:25:17 +0700

    refactor: remove user service module and associated files

--- microservices-order-system/user-service/src/main/java/com/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.userservice.repository;
-
-import com.userservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
